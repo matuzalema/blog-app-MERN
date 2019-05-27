785b5385407dce0474111643f5892306082d810e
matuzalema <matuzalema@gmail.com>
2019-05-27 15:41:11 +0200

    Add Home and About modules

---
+++ client/modules/About/About.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+}
+
+About.propTypes = {
+};
+
+export default About;
